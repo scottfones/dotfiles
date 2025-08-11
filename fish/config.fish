95a61b997326b433f4477c4746d08b913f225177
Scott Fones <scott.fones@gmail.com>
2025-08-11 14:27:47 -0400

    added dircolors sync

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,9 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# Set ls colors
+eval (dircolors -c)
+
 # source /home/scott/.python_general_venv/bin/activate.fish
 starship init fish | source
 zoxide init fish | source
