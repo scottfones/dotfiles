82d9fe3c61bf0deef37d0b92225a427d25741762
Scott Fones <scott.fones@gmail.com>
2025-08-17 08:42:37 -0400

    added bass source nvm and kanaawa theme

--- fish/config.fish
+++ fish/config.fish
@@ -5,6 +5,12 @@ end
 # Set ls colors
 eval (dircolors -c)
 
+# theme
+# source ~/.config/fish/themes/kanagawa.fish
+
+# nvm, npm, node 
+bass source ~/.nvm/nvm.sh
+
 # source /home/scott/.python_general_venv/bin/activate.fish
 starship init fish | source
 zoxide init fish | source
