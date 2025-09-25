0e625015de49d06e32f9a10bd33c6966e67e5921
Scott Fones <scott.fones@gmail.com>
2025-09-24 23:34:12 -0400

    init commit

---
+++ fish/functions/code.fish
@@ -0,0 +1,4 @@
+function code --description 'alias code code --force-device-scale-factor=1.5'
+ command code --force-device-scale-factor=1.5 $argv
+        
+end
---
+++ fish/functions/wezterm.fish
@@ -0,0 +1,4 @@
+function wezterm --wraps='/home/scott/Applications/Wezterm/wezterm/target/release/wezterm --config-file /home/scott/.config/wezterm/wezterm.lua' --description 'alias wezterm=/home/scott/Applications/Wezterm/wezterm/target/release/wezterm --config-file /home/scott/.config/wezterm/wezterm.lua'
+  /home/scott/Applications/Wezterm/wezterm/target/release/wezterm --config-file /home/scott/.config/wezterm/wezterm.lua $argv
+        
+end
