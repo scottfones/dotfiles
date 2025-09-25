634adcbc65f513f61c140e0366fc502bdd51be97
Scott Fones <scott.fones@gmail.com>
2025-09-24 23:39:10 -0400

    disabled bacon-ls. enabled pedantic and nursery clippy lints

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,12 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- LSP Server to use for Rust.
--- Set to "bacon-ls" to use bacon-ls instead of rust-analyzer.
--- only for diagnostics. The rest of LSP support will still be
--- provided by rust-analyzer.
-vim.g.lazyvim_rust_diagnostics = "bacon-ls"
-
 -- LSP Server to use for Python.
 -- Set to "basedpyright" to use basedpyright instead of pyright.
 vim.g.lazyvim_python_lsp = "basedpyright"
@@ -19,3 +13,22 @@ vim.o.guicursor = "n-v-c-sm-ve:block,r-cr-o-i-ci:hor20,a:blinkwait700-blinkoff40
 
 -- wl clipboard transient window fix
 vim.opt.clipboard = "unnamedplus"
+
+-- LSP Server to use for Rust.
+-- Set to "bacon-ls" to use bacon-ls instead of rust-analyzer.
+-- only for diagnostics. The rest of LSP support will still be
+-- provided by rust-analyzer.
+-- vim.g.lazyvim_rust_diagnostics = "bacon-ls"
+
+vim.g.rustaceanvim = {
+  server = {
+    default_settings = {
+      ["rust-analyzer"] = {
+        check = {
+          command = "clippy",
+          extraArgs = { "--all", "--", "-W", "clippy::all", "-W", "clippy::pedantic", "-W", "clippy::nursery" },
+        },
+      },
+    },
+  },
+}
