5daae9c62691c3aa5c4378dc7f6ba4142f6165fe
Scott Fones <scott.fones@gmail.com>
2025-10-10 15:08:23 -0400

    added uv plugin. added uv venv as python3 provider. lazy update

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,6 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- neovim python virtual environment
+vim.g.python3_host_prog = vim.fn.expand("~/.config/nvim/venv/bin/python")
 -- LSP Server to use for Python.
 -- Set to "basedpyright" to use basedpyright instead of pyright.
 vim.g.lazyvim_python_lsp = "basedpyright"
