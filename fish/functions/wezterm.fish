4b78367847b868699bcbcebe631226bfde6c5ac7
Scott Fones <scott.fones@gmail.com>
2026-04-09 14:08:54 -0400

    feat: wrapped wezterm to run on dGPU

--- fish/functions/wezterm.fish
+++ fish/functions/wezterm.fish
@@ -1,3 +1,3 @@
 function wezterm --description "WezTerm from local build"
-    /home/scott/Applications/Wezterm/wezterm/target/release/wezterm --config-file /home/scott/.config/wezterm/wezterm.lua $argv
+    prime-run /home/scott/Applications/Wezterm/wezterm/target/release/wezterm --config-file /home/scott/.config/wezterm/wezterm.lua $argv
 end
