c8048680b4e426547afdd29bee0802f70eb489df
Scott Fones <scott.fones@gmail.com>
2026-05-21 01:05:19 -0400

    feat: added tmux-nvim pane nav

---
+++ nvim/lua/plugins/tmux.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+    },
+    keys = {
+      { "<C-h>", "<cmd>TmuxNavigateLeft<cr>", desc = "Window/Pane left" },
+      { "<C-j>", "<cmd>TmuxNavigateDown<cr>", desc = "Window/Pane down" },
+      { "<C-k>", "<cmd>TmuxNavigateUp<cr>", desc = "Window/Pane up" },
+      { "<C-l>", "<cmd>TmuxNavigateRight<cr>", desc = "Window/Pane right" },
+    },
+  },
+}
